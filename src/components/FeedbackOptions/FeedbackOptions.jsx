20d4f86fef39238df092d00bf76f40ea63c86938
Andre29839 <133494235+Andre29839@users.noreply.github.com>
2023-08-29 19:40:11 +0300

    commit

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,13 +1,19 @@
-import css from "./FeedbackOptions.module.css"
+import css from './FeedbackOptions.module.css';
 
-export const FeedbackOptions = ({ option, onLeaveFeedback }) => { 
-    return (
-        <ul className={css.container}>
-            {option.map(option => (
-                <li className={css.btn_container} key={option}>
-                    <button className={css.btn_option} type="button" onClick={() => onLeaveFeedback(option)}>{ option }</button>
-                </li>
-            ))}
-        </ul>
-    )
-}
+export const FeedbackOptions = ({ option, onLeaveFeedback }) => {
+  return (
+    <ul className={css.container}>
+      {option.map(option => (
+        <li className={css.btn_container} key={option}>
+          <button
+            className={css.btn_option}
+            type="button"
+            onClick={() => onLeaveFeedback(option)}
+          >
+            {option}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
